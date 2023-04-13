258cd70667b5303216c2b7584bf01676b1e678a3
박민성 <minsung060607@gmail.com>
2023-04-13 22:12:52 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace study
+{
+    class Program
+    {
+        //java와 비슷한 구조를 알 수 잇으며 자바와 똑같이 class를 사용하는 것을 알 수 있다
+        //그러면서 for와 while,if는 c와 같다는 거을 알 수 있다
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World");
+        }
+    }
+}
